8da4e50df2638977fa0c0ed755ed1116f298eb03
VI-1911 <2571845421@qq.com>
2018-10-21 23:38:47 +0800

    .

--- gframe/image_manager.cpp
+++ gframe/image_manager.cpp
@@ -106,7 +106,7 @@ void ImageManager::RefreshRandomImageList() {
 	RefreshImageDir(L"cover2/", TEXTURE_COVER_O);
 	RefreshImageDir(L"attack/", TEXTURE_ATTACK);
 	RefreshImageDir(L"act/", TEXTURE_ACTIVATE);
-
+	RefreshImageDir(L"head/", TEXTURE_AVATAR_S);
 	for(int i = 0; i < 7; ++ i) {
 		saved_image_id[i] = -1;
 	}
