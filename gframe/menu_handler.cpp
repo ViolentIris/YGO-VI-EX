a5d1db8c04ea1c7ef16a5aaf87f4d60839440ef8
VI-1911 <2571845421@qq.com>
2019-02-27 10:36:32 +0800

    .

--- gframe/menu_handler.cpp
+++ gframe/menu_handler.cpp
@@ -120,29 +120,45 @@ bool MenuHandler::OnEvent(const irr::SEvent& event) {
 			case BUTTON_233: {
 				wcscpy(mainGame->gameConf.lasthost, L"s1.ygo233.com");
 				wcscpy(mainGame->gameConf.lastport, L"233");
+				const wchar_t* pstr = mainGame->ebJoinHost->getText();
+				BufferIO::CopyWStr(pstr, mainGame->gameConf.lasthost, 100);
+				BufferIO::CopyWStr(mainGame->ebJoinPort->getText(), mainGame->gameConf.lastport, 20);
 				mainGame->stACMessage->setText(dataManager.GetSysString(1440));
 				mainGame->PopupElement(mainGame->wACMessage, 20);
+				mainGame->HideElement(mainGame->wSC);
 				break;
 			}
 			case BUTTON_23333: {
 				wcscpy(mainGame->gameConf.lasthost, L"s1.ygo233.com");
 				wcscpy(mainGame->gameConf.lastport, L"23333");
+				const wchar_t* pstr = mainGame->ebJoinHost->getText();
+				BufferIO::CopyWStr(pstr, mainGame->gameConf.lasthost, 100);
+				BufferIO::CopyWStr(mainGame->ebJoinPort->getText(), mainGame->gameConf.lastport, 20);
 				mainGame->stACMessage->setText(dataManager.GetSysString(1440));
 				mainGame->PopupElement(mainGame->wACMessage, 20);
+				mainGame->HideElement(mainGame->wSC);
 				break;
 			}
 			case BUTTON_7210: {
 				wcscpy(mainGame->gameConf.lasthost, L"koishi.ygopro.cn");
 				wcscpy(mainGame->gameConf.lastport, L"7210");
+				const wchar_t* pstr = mainGame->ebJoinHost->getText();
+				BufferIO::CopyWStr(pstr, mainGame->gameConf.lasthost, 100);
+				BufferIO::CopyWStr(mainGame->ebJoinPort->getText(), mainGame->gameConf.lastport, 20);
 				mainGame->stACMessage->setText(dataManager.GetSysString(1440));
 				mainGame->PopupElement(mainGame->wACMessage, 20);
+				mainGame->HideElement(mainGame->wSC);
 				break;
 			}
 			case BUTTON_222: {
 				wcscpy(mainGame->gameConf.lasthost, L"koishi.ygopro.cn");
 				wcscpy(mainGame->gameConf.lastport, L"222");
+				const wchar_t* pstr = mainGame->ebJoinHost->getText();
+				BufferIO::CopyWStr(pstr, mainGame->gameConf.lasthost, 100);
+				BufferIO::CopyWStr(mainGame->ebJoinPort->getText(), mainGame->gameConf.lastport, 20);
 				mainGame->stACMessage->setText(dataManager.GetSysString(1440));
 				mainGame->PopupElement(mainGame->wACMessage, 20);
+				mainGame->HideElement(mainGame->wSC);
 				break;
 			}
 			case BUTTON_SC_EXIT: {
