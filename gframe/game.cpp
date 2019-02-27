91ecb0bb27fa6a71e35b6c1afd239df07589fc70
VI-1911 <2571845421@qq.com>
2019-02-27 20:16:50 +0800

    .

--- gframe/game.cpp
+++ gframe/game.cpp
@@ -441,7 +441,7 @@ bool Game::Initialize() {
 	stMessage->setTextAlignment(irr::gui::EGUIA_UPPERLEFT, irr::gui::EGUIA_CENTER);
 	btnMsgOK = env->addButton(rect<s32>(130, 105, 220, 130), wMessage, BUTTON_MSG_OK, dataManager.GetSysString(1211));
 	//auto fade message (310)
-	wACMessage = env->addWindow(rect<s32>(460, 260, 810, 320), false, L"");
+	wACMessage = env->addWindow(rect<s32>(460, 240, 810, 300), false, L"");
 	wACMessage->getCloseButton()->setVisible(false);
 	wACMessage->setVisible(false);
 	wACMessage->setDrawBackground(false);
@@ -1896,7 +1896,7 @@ void Game::OnResize() {
 	wHand->setRelativePosition(ResizeWin(500, 450, 825, 605));
 	wFTSelect->setRelativePosition(ResizeWin(550, 240, 780, 340));
 	wMessage->setRelativePosition(ResizeWin(490, 200, 840, 340));
-	wACMessage->setRelativePosition(ResizeWin(490, 240, 840, 300));
+	wACMessage->setRelativePosition(ResizeWin(460, 240, 810, 300));
 	wQuery->setRelativePosition(ResizeWin(490, 200, 840, 340));
 	wSurrender->setRelativePosition(ResizeWin(490, 200, 840, 340));
 	wOptions->setRelativePosition(ResizeWin(490, 200, 840, 340));
