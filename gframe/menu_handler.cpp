2a27fce30bc0fb0a116770822fb406a4bbf1fa74
VI-1911 <2571845421@qq.com>
2019-04-09 13:34:13 +0800

    .

--- gframe/menu_handler.cpp
+++ gframe/menu_handler.cpp
@@ -114,7 +114,6 @@ bool MenuHandler::OnEvent(const irr::SEvent& event) {
 				break;
 			}
 			case BUTTON_SYS_EXIT: {
-				mainGame->device->closeWindow();
 				mainGame->SaveConfig();
 				mainGame->Initialize();
 				if(exit_on_return)
