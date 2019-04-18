7442b31804636dc4b5bb7b75f6c628546e778c0c
VI-1911 <2571845421@qq.com>
2019-04-18 19:55:55 +0800

    .

--- gframe/game.cpp
+++ gframe/game.cpp
@@ -153,7 +153,7 @@ bool Game::Initialize() {
 	lstHostList->setItemHeight(18);
 	btnLanRefresh = env->addButton(rect<s32>(180, 325, 280, 350), wLanWindow, BUTTON_LAN_REFRESH, dataManager.GetSysString(1217));
 	env->addStaticText(dataManager.GetSysString(1221), rect<s32>(10, 360, 220, 380), false, false, wLanWindow);
-	btnSC = env->addButton(rect<s32>(320, 325, 420, 350), wLanWindow, BUTTON_SERVER_CHOOSE, dataManager.GetSysString(1434));
+	btnSC = env->addButton(rect<s32>(320, 325, 420, 350), wLanWindow, BUTTON_SERVER_CHOOSE, dataManager.GetSysString(1427));
 	ebJoinHost = env->addEditBox(gameConf.lasthost, rect<s32>(110, 355, 350, 380), true, wLanWindow);
 	ebJoinHost->setTextAlignment(irr::gui::EGUIA_CENTER, irr::gui::EGUIA_CENTER);
 	ebJoinPort = env->addEditBox(gameConf.lastport, rect<s32>(360, 355, 420, 380), true, wLanWindow);
