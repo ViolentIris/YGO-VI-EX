a38593fada3bf6ab24949f937b7c59791f9401d2
VI-1911 <2571845421@qq.com>
2019-05-26 20:42:54 +0800

    .

--- gframe/menu_handler.cpp
+++ gframe/menu_handler.cpp
@@ -129,8 +129,6 @@ bool MenuHandler::OnEvent(const irr::SEvent& event) {
 				mainGame->cbFont->setEnabled(true);
 				mainGame->cbLocale->setEnabled(true);
 				mainGame->btnHeadS->setEnabled(true);
-				mainGame->btnCoverS->setEnabled(true);
-				mainGame->btnBgS->setEnabled(true);
 				mainGame->btnSystemExit->setEnabled(true);
 				mainGame->HideElement(mainGame->wMainMenu);
 				mainGame->ShowElement(mainGame->wSystem);
