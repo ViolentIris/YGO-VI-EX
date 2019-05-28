d3bccd53e94ba6f777c1554db8e6a0fbe60b7456
VI-1911 <2571845421@qq.com>
2019-05-28 21:45:53 +0800

    .

--- gframe/event_handler.cpp
+++ gframe/event_handler.cpp
@@ -816,7 +816,7 @@ bool ClientField::OnEvent(const irr::SEvent& event) {
 				WideCharToMultiByte(CP_ACP, 0, self1, -1, sbuf1, sLength1, NULL, NULL);
 				char sbuffer1[300];
 				sprintf(sbuffer1, "L\".\\textures\\head\\s%\"", sbuf1);
-				CopyFile(self1, L".\\textures\\output.jpg", FALSE);
+				CopyFile(sbuffer1, L".\\textures\\output.jpg", FALSE);
 				break;
 			}
 			}
