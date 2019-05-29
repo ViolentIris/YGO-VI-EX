d840968eec3524c09f236f68fce90d34a1b38527
VI-1911 <2571845421@qq.com>
2019-05-29 12:48:26 +0800

    .

--- gframe/event_handler.cpp
+++ gframe/event_handler.cpp
@@ -1874,8 +1874,8 @@ bool ClientField::OnCommonEvent(const irr::SEvent& event) {
 				break;
 			}
 			case BUTTON_HDS_OK: {
-				wchar_t self1[2048] = 0;
-				myswprintf(self1, L"./textures/head/%ls", mainGame->cbFont->getItem(mainGame->cbFont->getSelected()))
+				wchar_t self1[2048];
+				myswprintf(self1, L"./textures/head/%ls", mainGame->cbHDS->getItem(mainGame->cbHDS->getSelected()));
 				CopyFile(self1, L"./textures/output.jpg", FALSE);
 				break;
 			}
