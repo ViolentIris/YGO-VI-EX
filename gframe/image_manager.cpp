aaa9e48a9731b1741b27cd6d59e558e1d443bf22
VI-1911 <2571845421@qq.com>
2019-05-30 20:48:47 +0800

    .

--- gframe/image_manager.cpp
+++ gframe/image_manager.cpp
@@ -73,9 +73,7 @@ bool ImageManager::Initial()  {
 	tLim = driver->getTexture("textures/lim.png");
 	tOT = driver->getTexture("textures/ot.png");
 	tCardType = driver->getTexture("textures/cardtype.png");
-		wchar_t self1[2048];
-		myswprintf(self1, L"\"textures/head/%ls\"", mainGame->cbHDS->getItem(mainGame->cbHDS->getSelected()));
-	tHDS = driver->getTexture(self1);
+	tHDS = driver->getTexture("textures/head/head_01.jpg");
 	tHand[0] = driver->getTexture("textures/f1.jpg");
 	tHand[1] = driver->getTexture("textures/f2.jpg");
 	tHand[2] = driver->getTexture("textures/f3.jpg");
