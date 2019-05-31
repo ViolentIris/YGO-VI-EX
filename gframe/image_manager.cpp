2b5ffa88f8386a6210251fe70351f7b5e857cda7
VI-1911 <2571845421@qq.com>
2019-05-31 22:34:08 +0800

    .

--- gframe/image_manager.cpp
+++ gframe/image_manager.cpp
@@ -132,10 +132,11 @@ bool ImageManager::Initial()  {
 		snprintf(buff, 100, "textures/pscale/lscale_%d.png", i);
 		tLScale[i] = driver->getTexture(buff);
 	}	
-	char self1[2048];
-		mainGame->cbHDS->setSelected(111);
-		snprintf(self1, "textures/head/%s", mainGame->cbHDS->getItem(mainGame->cbHDS->getSelected()));
-	tHDS = driver->getTexture(self1);
+	char SName1[1024];
+		wchar_t Sfname1[1024];
+		myswprintf(Sfname1, L"./textures/head/%ls", mainGame->cbHDS->getItem(mainGame->cbHDS->getSelected()));
+		BufferIO::EncodeUTF8(Sfname1, SName1);
+	tHDS = driver->getTexture(SName1);
 	support_types.push_back(std::string("jpg"));
 	support_types.push_back(std::string("png"));
 	support_types.push_back(std::string("bpg"));
