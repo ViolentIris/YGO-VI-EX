682908ea40bba4f39fd37c518a847cf55de72a5a
VI-1911 <2571845421@qq.com>
2019-08-20 20:03:54 +0800

    .

--- gframe/menu_handler.cpp
+++ gframe/menu_handler.cpp
@@ -372,7 +372,7 @@ bool MenuHandler::OnEvent(const irr::SEvent& event) {
 				break;
 			}
 			case BUTTON_RM1: {
-				const wchar_t* pstr = mainGame->ebJoinPass->getText();
+				wchar_t pstr = mainGame->ebJoinPass->getText();
 				char a[10]="M";
 				strcat(pstr, a);
 				wchar_t buff[1024];
