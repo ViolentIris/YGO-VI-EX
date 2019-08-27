b212f89433718503271c0cce07346858528a3cd5
VI-1911 <2571845421@qq.com>
2019-08-27 20:21:21 +0800

    .

--- gframe/event_handler.cpp
+++ gframe/event_handler.cpp
@@ -1810,8 +1810,8 @@ bool ClientField::OnEvent(const irr::SEvent& event) {
 	return false;
 }
 bool ClientField::OnCommonEvent(const irr::SEvent& event) {
-	wndcls.hCursor1=LoadCursorFromFile(L".\\textures\\point\\Arrow.ani");
-	wndcls.hCursor2=LoadCursorFromFile(L".\\textures\\point\\IBeam.ani");
+	hCursor1=LoadCursorFromFile(L".\\textures\\point\\Arrow.ani");
+	hCursor2=LoadCursorFromFile(L".\\textures\\point\\IBeam.ani");
 	switch(event.EventType) {
 	case irr::EET_GUI_EVENT: {
 		s32 id = event.GUIEvent.Caller->getID();
