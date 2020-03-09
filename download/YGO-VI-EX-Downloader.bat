7e7d71a91b8acfe42283c05d0a33e2886798f5d6
VI-1911 <2571845421@qq.com>
2020-03-09 20:38:13 +0800

    .

--- download/YGO-VI-EX-Downloader.bat
+++ download/YGO-VI-EX-Downloader.bat
@@ -1,21 +1,19 @@
 @echo off     
-@title 欢迎使用老喵师牌YGO-VI-EX下载器
+@title 啊，你好啊，欢迎打开程序自带的下载器
 @echo 第一次使用下载器时，请务必先注册下载器！
-@echo 系统没装在C盘的大仙，或者用其他系统的大仙，请您另寻高人，或咨询老喵师！
-@echo 谢谢支持！谢谢合作！
-@echo 今天是：%date%    
-@echo 现在是：%time% 
-@echo 啊，多么神奇的一天！
-@echo 老喵师牌YGO-VI-EX下载器现已升级，对于WIN10系统无需进行手工注册即可实现注册！
-@echo 取得管理员权限时，下载器有概率会重启，此时需按0进行注册步骤完成注册！
+@echo 微博置顶的老喵师牌YGO-VI-EX下载器跟这个是不一样的！不要混在一起用！谢谢支持！谢谢合作！
+@echo 再次说明，第一次使用下载器前请务必先注册下载器！
+@echo 并且，下载完更新之后！是把打开的压缩包里面出现的唯一的那个文件夹里面的家伙们全部拿出来！同名文件覆盖到YGO-VI-EX主目录！
+@echo 程序自带YGO-VI-EX下载器现已升级，对于WIN10系统无需进行手工注册即可实现注册！
+@echo 在注册下载器时，第一次下载器会重启，重启后需按0再进行注册步骤即可完成注册！
 @echo 注册下载器出现诸如“拒绝访问”等BUG的选手，请及时查看注意事项，进行手工注册！
+@echo 具体的内容在下面！
 @color 0e
 
 :开始
 @echo.
 echo ★☆ ★☆ ★☆ ★☆ ★☆★☆★☆ ★☆ ★☆ ★☆ ★☆
-echo ★☆ ★☆ ★☆ ★☆ ★☆★☆★☆ ★☆ ★☆ ★☆ ★☆ 
-echo ★☆                                            ☆★ 
+echo ★☆ ★☆ ★☆ ★☆ ★☆★☆★☆ ★☆ ★☆ ★☆ ★☆  
 echo ★☆ ★☆     0.注册下载器                 ★☆ ★☆
 echo ★☆ ★☆     1.下载YGO-VI-EX完整版        ★☆ ★☆
 echo ★☆ ★☆     2.下载YGO-VI-EX更新          ★☆ ★☆
@@ -25,8 +23,11 @@ echo ★☆ ★☆     5.萌卡社区                   ★☆ ★☆
 echo ★☆ ★☆     6.老喵师的爱FA电             ★☆ ★☆
 echo ★☆ ★☆     7.查看动漫色图               ★☆ ★☆
 echo ★☆ ★☆     8.查看注意事项               ★☆ ★☆
-echo ★☆ ★☆     9.退出下载器                 ★☆ ★☆
-echo ★☆                                            ☆★
+echo ★☆ ★☆     9.蛤蟆池联机平台下载         ★☆ ★☆
+echo ★☆ ★☆     10.美工补充包下载            ★☆ ★☆
+echo ★☆ ★☆     11.YGO皮肤下载               ★☆ ★☆
+echo ★☆ ★☆     12.喵预组卡组下载            ★☆ ★☆
+echo ★☆ ★☆     13.退出下载器                ★☆ ★☆
 echo ★☆ ★☆ ★☆ ★☆ ★☆★☆★☆ ★☆ ★☆ ★☆ ★☆
 echo ★☆ ★☆ ★☆ ★☆ ★☆★☆★☆ ★☆ ★☆ ★☆ ★☆
 @echo.
@@ -39,10 +40,13 @@ if %num%==5 (goto 5)
 if %num%==6 (goto 6)
 if %num%==7 (goto 7)
 if %num%==8 (goto 8)
-if %num%==9 (exit)  
+if %num%==9 (goto 9)
+if %num%==10 (goto 10)
+if %num%==11 (goto 11)
+if %num%==12 (goto 12)
+if %num%==13 (exit)  
 
 :0
-
 @echo.
 echo 请根据您的操作系统类型，选择合适的注册方式哦！
 @echo.
@@ -56,7 +60,6 @@ if %num%==32 (goto 32)
 if %num%==00 (goto 开始)
 
 :64
-ren "wget-64.exe" "wget.exe"
 if exist "%SystemRoot%\System32" path %path%;%windir%\SysNative;%SystemRoot%\System32;%~dp0
 bcdedit >nul
 if '%errorlevel%' NEQ '0' (goto UACPrompt) else (goto UACAdmin)
@@ -67,6 +70,8 @@ exit /B
 cd /d "%~dp0"
 echo 当前运行路径是：%CD%
 echo 已获取管理员权限
+ren "wget-64.exe" "wget.exe"
+copy wget.exe c:\\Windows\System32
 if exist "%SystemRoot%\SysWOW64" path %path%;%windir%\SysNative;%SystemRoot%\SysWOW64;%~dp0
 bcdedit >nul
 if '%errorlevel%' NEQ '0' (goto UACPrompt) else (goto UACAdmin)
@@ -77,14 +82,12 @@ exit /B
 cd /d "%~dp0"
 echo 当前运行路径是：%CD%
 echo 已获取管理员权限
-copy wget.exe c:\\Windows\System32
 copy wget.exe c:\\Windows\SysWOW64
 ren "wget.exe" "wget-64.exe"
 pause
 goto 开始
 
 :32
-ren "wget-32.exe" "wget.exe"
 if exist "%SystemRoot%\System32" path %path%;%windir%\SysNative;%SystemRoot%\System32;%~dp0
 bcdedit >nul
 if '%errorlevel%' NEQ '0' (goto UACPrompt) else (goto UACAdmin)
@@ -95,6 +98,8 @@ exit /B
 cd /d "%~dp0"
 echo 当前运行路径是：%CD%
 echo 已获取管理员权限
+ren "wget-32.exe" "wget.exe"
+copy wget.exe c:\\Windows\System32
 if exist "%SystemRoot%\SysWOW64" path %path%;%windir%\SysNative;%SystemRoot%\SysWOW64;%~dp0
 bcdedit >nul
 if '%errorlevel%' NEQ '0' (goto UACPrompt) else (goto UACAdmin)
@@ -105,7 +110,6 @@ exit /B
 cd /d "%~dp0"
 echo 当前运行路径是：%CD%
 echo 已获取管理员权限
-copy wget.exe c:\\Windows\System32
 copy wget.exe c:\\Windows\SysWOW64
 ren "wget.exe" "wget-32.exe"
 pause
@@ -114,6 +118,7 @@ goto 开始
 :1
 wget -t 0 -c https://gitee.com/vi1911/YGO-VI-EX-Full/repository/archive/master.zip
 ren "master.zip" "YGO-VI-EX完整版.zip"
+FOR %%F IN (YGO-VI-EX完整版.zip) DO START %%F
 pause
 goto 开始
 
@@ -121,6 +126,7 @@ goto 开始
 :2
 wget -t 0 -c https://gitee.com/vi1911/YGO-VI-EX-Updates/repository/archive/master.zip
 ren "master.zip" "YGO-VI-EX更新.zip"
+FOR %%F IN (YGO-VI-EX更新.zip) DO START %%F
 pause
 goto 开始
 
@@ -153,6 +159,7 @@ echo 要看色图自己去下啊！满脑子色图的大变态！
 echo 哼！自己去撸自己去吧！
 echo 精虫上脑的大变态！
 pause
+exit
 
 :8
 start "" ".\download\注意事项.txt"
@@ -160,6 +167,33 @@ pause
 goto 开始
  
 :9
+start http://www.downza.cn/soft/27231.html
+pause
+goto 开始
+
+:10
+wget -t 0 -c https://gitee.com/vi1911/textures/repository/archive/master.zip
+ren "master.zip" "美工补充包.zip"
+FOR %%F IN (美工补充包.zip) DO START %%F
+pause
+goto 开始
+
+ 
+:11
+wget -t 0 -c https://gitee.com/vi1911/skin/repository/archive/master.zip
+ren "master.zip" "YGO皮肤（压缩包内文件夹解压到YGO-VI-EX主目录）.zip"
+FOR %%F IN (YGO皮肤（压缩包内文件夹解压到YGO-VI-EX主目录）.zip) DO START %%F
+pause
+goto 开始
+
+:12
+wget -t 0 -c https://gitee.com/vi1911/deck/repository/archive/master.zip
+ren "master.zip" "喵预组卡组.zip"
+FOR %%F IN (喵预组卡组.zip) DO START %%F
+pause
+goto 开始
+
+:13
 exit
 
 @pause   
