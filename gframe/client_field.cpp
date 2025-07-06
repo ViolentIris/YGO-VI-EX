8547c614505bcecfd4128869a520b9b1336c640e
VI-1911 <2571845421@qq.com>
2025-07-06 15:16:21 +0800

    .

--- gframe/client_field.cpp
+++ gframe/client_field.cpp
@@ -1590,17 +1590,17 @@ void ClientField::UpdateDeclarableList() {
 				ancard.push_back(cache_code);
 			}
 		}
-		if(ancard.size())
-			return;
+		// if(ancard.size())
+		// return;
 	}
-	for(auto cit = dataManager.strings_begin; cit != dataManager.strings_end; ++cit) {
+	for(auto cit = dataManager.strings_begin(); cit != dataManager.strings_end(); ++cit) {
 		if(cit->second.name.find(pname) != std::wstring::npos || mainGame->CheckRegEx(cit->second.name, pname)) {
-			auto cp = dataManager.GetCodePointer(cit->first);	//verified by _strings
-			if (cp == dataManager.datas_end)
+			auto cp = dataManager.GetCodePointer(cit->first);
+			if (cp == dataManager.datas_end())
 				continue;
 			//datas.alias can be double card names or alias
 			if(is_declarable(cp->second, declare_opcodes)) {
-				if(pname == cit->second.name || mainGame->CheckRegEx(cit->second.name, pname, true)) { //exact match
+				if(pname == cit->second.name) { //exact match
 					mainGame->lstANCard->insertItem(0, cit->second.name.c_str(), -1);
 					ancard.insert(ancard.begin(), cit->first);
 				} else {
