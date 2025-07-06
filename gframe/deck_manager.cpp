e546d2dc6ee306e2958783ce0b71508cca010d76
VI-1911 <2571845421@qq.com>
2025-07-06 10:18:34 +0800

    .

--- gframe/deck_manager.cpp
+++ gframe/deck_manager.cpp
@@ -1,5 +1,6 @@
 #include "deck_manager.h"
 #include "data_manager.h"
+#include "myfilesystem.h"
 #include "network.h"
 #include "game.h"
 
@@ -225,11 +226,6 @@ bool DeckManager::LoadSide(Deck& deck, int* dbuf, int mainc, int sidec) {
 FILE* DeckManager::OpenDeckFile(const wchar_t* file, const char* mode) {
 #ifdef WIN32
 	FILE* fp = mywfopen(file, mode);
-#else
-	char file2[256];
-	BufferIO::EncodeUTF8(file, file2);
-	FILE* fp = fopen(file2, mode);
-#endif
 	return fp;
 }
 bool DeckManager::LoadDeck(const wchar_t* file) {
